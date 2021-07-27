785c18e3e3bf584d75abdabdd54644997ae6b160
nnizhang <32151253+nnizhang@users.noreply.github.com>
2021-07-27 21:13:44 +0800

    Add files via upload

--- parameter.py
+++ parameter.py
@@ -1,22 +1,21 @@
 import os
 
 ################# Training #################
-# path for pretrained vgg model
-# load_model = './pretrained_models/vgg16_20M.caffemodel.pth'
-load_model = '/home/zhangni/pretrained_models/vgg16_20M.caffemodel.pth'
-# path for COCO9213
-img_root_coco = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/COCO9213-os/img/'
-gt_root_coco = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/COCO9213-os/gt/'
-# path for DUTS Class
-img_root = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/DUTS_class/img/'
-gt_root = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/DUTS_class/gt/'
-# path for our synethsis data
-img_syn_root = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/DUTS_class_syn/img_png_seamless_cloning_add_naive/img/'
-img_ReverseSyn_root = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/DUTS_class_syn/img_png_seamless_cloning_add_naive_reverse/img/'
-gt_ReverseSyn_root = '/home/zhangni/zhangni/Data/Co-Saliency/trainset/DUTS_class_syn/img_png_seamless_cloning_add_naive_reverse/gt/'
+# Your path for pretrained vgg model
+load_model = './pretrained_models/vgg16_20M.caffemodel.pth'
+# Your path for COCO9213
+img_root_coco = './Data/COCO9213-os/img/'
+gt_root_coco = './Data/COCO9213-os/gt/'
+# Your path for DUTS Class
+img_root = './Data/DUTS_class/img/'
+gt_root = './Data/DUTS_class/gt/'
+# Your path for our synethsis data
+img_syn_root = './Data/DUTS_class_syn/img_png_seamless_cloning_add_naive/img/'
+img_ReverseSyn_root = './Data/DUTS_class_syn/img_png_seamless_cloning_add_naive_reverse/img/'
+gt_ReverseSyn_root = './Data/DUTS_class_syn/img_png_seamless_cloning_add_naive_reverse/gt/'
 
 # save model path
-save_model_dir = 'checkpoint/'
+save_model_dir = './checkpoint/'
 if not os.path.exists(save_model_dir):
     os.makedirs(save_model_dir)
 
@@ -36,12 +35,11 @@ stepvalue1 = 20000
 stepvalue2 = 30000
 loss_weights = [1, 0.8, 0.8, 0.5, 0.5, 0.5]
 bn_momentum = 0.001
-save_steps = [20000, 30000, 35000, 38000, 39000]
 
 
 ################# Testing #################
 # save output path
-save_test_path_root = 'Preds/'
+save_test_path_root = './Preds/'
 if not os.path.exists(save_test_path_root):
     os.makedirs(save_test_path_root)
 
@@ -53,9 +51,9 @@ test_model_dir = save_model_dir + test_model
 # test_model_dir = 'checkpoint/CADC.pth'
 
 # test dataset path
-test_dir_img = ['/home/zhangni/zhangni/Data/Co-Saliency/dataset/CoCA/image',
-                '/home/zhangni/zhangni/Data/Co-Saliency/dataset/CoSal2015/Image',
-                '/home/zhangni/zhangni/Data/Co-Saliency/dataset/CoSOD3k/Image',
-                '/home/zhangni/zhangni/Data/Co-Saliency/dataset/MSRC/Image',
+test_dir_img = ['./Data/CoCA/image',
+                './Data/CoSal2015/Image',
+                './Data/CoSOD3k/Image',
+                './Data/MSRC/Image',
                 ]
 
